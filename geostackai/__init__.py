900166eebe6e85db1776e684f18980fc35a84f19
Jean-Sébastien Gosselin <jean-sebastien.gosselin@outlook.com>
2022-10-06 09:46:12 -0400

    Create __init__.py

---
+++ geostackai/__init__.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+# =============================================================================
+# Copyright (C) Les solutions géostack, Inc - All Rights Reserved
+#
+# This file is part of geostackai
+# Unauthorized copying of this file, via any medium is strictly prohibited
+# Proprietary and confidential
+#
+# https://github.com/geo-stack/geostackai
+# =============================================================================
+
+version_info = (0, 0, 1, 'dev0')
+__version__ = '.'.join(map(str, version_info))
+__project_url__ = "https://github.com/geo-stack/geostackai"
